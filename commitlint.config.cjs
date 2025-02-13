89a743c7321cad3478d5cb01f7bb558de5dca30c
Hung Pham Do <phamdohung161@gmail.com>
2025-02-13 11:24:08 +0700

    chore: 🔧 increase body-max-line-length limit to 1000 characters

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,5 +6,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', typeEnums],
+    'body-max-line-length': [2, 'always', 1000],
   },
 };
